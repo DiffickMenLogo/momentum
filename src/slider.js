97ae7478fe9685ed79cfdc441a4ebb33622abd84
DiffickMenLogo <gh.sdfs@mail.ru>
2021-10-30 16:00:50 +0300

    feat: end develop slider.js

--- src/slider.js
+++ src/slider.js
@@ -6,24 +6,34 @@ function getRandomInt(min, max) {
 }
 
 var randomNum = getRandomInt(1,20);
-const bgNum = randomNum.toString().padStart(2,'0');
 const body = document.querySelector('body');
 
 
 export function setBg() {
-    body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
+    const bgNum = randomNum.toString().padStart(2,'0');
+    const img = new Image();
+    img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+    img.onload = () => {
+        body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
+    }
 }
 
 export function getSlideNext(){
-    if(randomNum == 20){
+    if(randomNum != 20){
+        randomNum ++;
+    }
+    else{
         randomNum = 1;
     }
-    randomNum ++;
+    setBg();
 }
 
 export function getSlidePerv(){
-    if(randomNum == 1){
+    if(randomNum != 1){
+        randomNum --;
+    }
+    else{
         randomNum = 20;
     }
-    randomNum --;
+    setBg();
 }
